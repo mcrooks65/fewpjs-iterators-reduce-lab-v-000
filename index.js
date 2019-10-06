cfb91fa4cd61151f6802d67923b9d56be55e83e3
Matthew Crooks <mfcrooks@gmail.com>
2019-10-06 22:58:18 +0000

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
-const totalBatteries = batteryBatches.reduce((total, batch) => total + batch);
+// classic function version
+const totalBatteries = batteryBatches.reduce(function (total, batch) {
+  return total + batch;
+} );
+
+// arrow function version
+const totalBatteriesArrow = batteryBatches.reduce((total, batch) => total + batch, 0);
